10aeaae9cde533efbcbd540abce65fe3d4316680
DESKTOP-47LC7FK\shulm <shulmanbusiness@gmail.com>
2018-04-25 00:00:05 -0400

    no message

---
+++ Shards of Roh/Assets/Scripts/GameLogic/AI/Behaviours/Behaviours.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Behaviours {
+
+	protected string name;
+
+	public abstract void enact(UnitContainer unitInfo);
+
+	public string getName()
+	{
+		return name;
+	}
+}
---
+++ Shards of Roh/Assets/Scripts/GameLogic/AI/Behaviours/Retaliate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Retaliate : Behaviours {
+
+	public Retaliate () {
+
+	}
+
+	public override void enact (UnitContainer unitInfo) {
+
+	}
+}
